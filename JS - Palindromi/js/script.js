b8a85997ea89bfc4952afaf16f5aab63ec0749b5
Salvo-Raia <salvo.raia@outlook.it>
2026-01-09 00:57:32 +0100

    Add palindrome word check function

--- JS - Palindromi/js/script.js
+++ JS - Palindromi/js/script.js
@@ -5,13 +5,15 @@ console.log("Mai sentito parlare di palindromi? Si tratta di parole che si leggo
 console.log("Inserisci una parola palindroma, vediamo se hai afferrato il concetto...");
 
 let userWordInsert = prompt("Inserisci una parola che ritieni palindroma", "non ti suggerisco nulla...")
-console.log("Hai scritto", userWordInsert);
 const userWordReversed = userWordInsert.split("").reverse().join(""); 
+console.log("Hai scritto", userWordInsert);
 console.log("Che al contrario è:", userWordReversed);
 
-
 /* Creare una funzione per capire se la parola inserita è palindroma */ 
-
+function AbbafyMe (userWordInsert, userWordReversed) {
+let isTheWordPalindromeResponse = (userWordInsert.toLowerCase()) === (userWordReversed.toLowerCase()) ? true : false;
+return isTheWordPalindromeResponse;
+}
 
 
 
