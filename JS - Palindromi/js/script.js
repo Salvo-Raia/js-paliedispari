e837134ece531e31f8a3960a0918dc94273c3384
Salvo-Raia <salvo.raia@outlook.it>
2026-01-09 01:22:28 +0100

    Add Json DOC to function

--- JS - Palindromi/js/script.js
+++ JS - Palindromi/js/script.js
@@ -1,5 +1,3 @@
-console.log("Js is ready!");
-
 /* Chiedere all’utente di inserire una parola */
 console.log("Mai sentito parlare di palindromi? Si tratta di parole che si leggono alla stessa maniera da ambo i lati!");
 console.log("Inserisci una parola palindroma, vediamo se hai afferrato il concetto...");
@@ -9,16 +7,24 @@ const userWordReversed = userWordInsert.split("").reverse().join("");
 console.log("Hai scritto", userWordInsert);
 console.log("Che al contrario è:", userWordReversed);
 
-AbbafyMe (userWordInsert, userWordReversed);
-isTheWordPalindrome = isTheWordPalindromeResponse;
-console.log(isTheWordPalindrome);
-let wordMatch = (isTheWordPalindrome) ? "Sì, la parola è palindroma" : "No, questa parola non è palindroma"
+isTheWordPalindromeResponse = AbbafyMe (userWordInsert, userWordReversed);
+console.log(isTheWordPalindromeResponse);
+
+let wordMatch = (isTheWordPalindromeResponse) ? "Sì, la parola è palindroma" : "No, questa parola non è palindroma"
 console.log(wordMatch);
 
+//* Funzioni
 /* Creare una funzione per capire se la parola inserita è palindroma */ 
+/**
+ * This function check if a word is exactly the same when reversed. If this is the case, it returns a true value;
+ * @param {string} userWordInsert is a string prompted by user. 
+ * @param {string} userWordReversed is the same string prompted by user, but reversed.
+ * @const {boolean} isTheWordPalindromeResponse check if both the regular and reversed string matches as the same word and set true or false;
+ * @returns {boolean} 
+ */
 function AbbafyMe (userWordInsert, userWordReversed) {
-isTheWordPalindromeResponse = (userWordInsert.toLowerCase()) === (userWordReversed.toLowerCase()) ? true : false;
-return isTheWordPalindromeResponse;
+const isTheWordPalindrome = (userWordInsert.toLowerCase()) === (userWordReversed.toLowerCase()) ? true : false;
+return isTheWordPalindrome;
 }
 
 
